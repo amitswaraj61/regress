77a7765197a2c5aee9cc7a71fcc5fda7bd734d83
amitswaraj61 <aswaraj78@gmail.com>
2020-03-27 15:25:11 +0530

    uc4 is uploaded

--- uc1.sh
+++ uc1.sh
@@ -1,11 +1,13 @@
 #! /bin/bash -x
 shopt -s extglob
-echo "enter the word"
-read word
-pat=^[abc]{3}\.[a-zA-Z]{3}$
-if [[ $word =~ $pat ]]
-then
-   echo valid
+echo "enter the email"
+read email
+pat=^[a-z]{3}\.[a-zA-Z]{3}\@[a-zA-Z]{3,10}\.[co]{2}\.[a-z]{2}$
+if [[ $email =~ $pat ]]
+ then
+   echo email valid
 else
-   echo invalid
+   echo email invalid
 fi
+
+
