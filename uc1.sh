654b9ab4729dcd25a7e7c2bcbdec8c8e009a03b2
amitswaraj61 <aswaraj78@gmail.com>
2020-03-27 15:00:50 +0530

    uc2 uploaded

--- uc1.sh
+++ uc1.sh
@@ -2,10 +2,11 @@
 shopt -s extglob
 echo "enter the word"
 read word
-pat=^[abc]{3}\.[a-zA-Z]{3}$
+pat=^[abc]{3}\.[a-zA-Z]{3}\@[a-zA-Z]{3,10}$
 if [[ $word =~ $pat ]]
 then
    echo valid
 else
    echo invalid
 fi
+
